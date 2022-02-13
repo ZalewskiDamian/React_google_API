98259b0e0230e6f393fb36d90ad1b7c7398f449d
ZalewskiDamian <damian.github@gmail.com>
2022-02-13 17:07:57 +0100

    add navbar and dark mode theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import { Routes } from './components/Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import  Navbar  from './components/Navbar';
+import  Footer  from './components/Footer';
+import  Routes  from './components/Routes';
 
 const App = () => {
     const [darkTheme, setDarkTheme] = useState(false);
     
     return (
-        <div className={darkTheme ? 'dark' : 'light'}>
-            <h1>App</h1>
-        </div>
+        <Router>
+            <div className={darkTheme ? 'dark' : ''}>
+                <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                    <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                    <Routes />
+                    <Footer />
+                </div>
+            </div>
+        </Router>
     )
 }
 
